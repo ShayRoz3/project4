7ce1f2c90fabe77a6eb5b43ef17373334dd8b4d9
shay rozner <shayrozner@gmail.com>
2022-06-13 19:46:49 +0300

    13th

--- EX_CLASS_10.py
+++ EX_CLASS_10.py
@@ -15,7 +15,7 @@ actress = ['Rachel McAdams', 'Angelina Jolie', 'Gal Gadot',
 # ex 1
 print([movies + 'is played by ' + actress for (movies, actress) in zip(movies, actress)])
 # tha lambada way
-print(list(map(lambda x, y: x +'is played by ' + y, movies, actress)))
+print(list(map(lambda x, y: x + 'is played by ' + y, movies, actress)))
 
 # # ex2
 # movie = dict(zip(movies, actress))
@@ -23,7 +23,7 @@ print(list(map(lambda x, y: x +'is played by ' + y, movies, actress)))
 # # worked- ex2 the long way
 d = ({movies: actress for (movies, actress) in zip(movies, actress)})
 print(d)
-print(list(map(lambda x,y: x + ': ' + y, movies, actress)))
+print(list(map(lambda x, y: x + ': ' + y, movies, actress)))
 # i think that's what they ment- lambada way
 
 # ex3
@@ -32,18 +32,26 @@ print([movies + ' is played by ' + actress for (movies, actress) in d.items()])
 # she codes way
 
 # ex4
-[print(i * 100) for i in range(1,10) if i%2==0]
+[print(i * 100) for i in range(1, 10) if i % 2 == 0]
 # success by myself
-print(list(map(lambda x: x*100 if x%2==0 else "-", range(1,10))))
+print(list(map(lambda x: x * 100 if x % 2 == 0 else "-", range(1, 10))))
 
 # ex5
 # [print(x*100) if x%2==0 else print(x) for x in range(1,10)]
 # easy breasy
-print(list(map(lambda x: x*100 if x%2==0 else x, range(1,10))))
+print(list(map(lambda x: x * 100 if x % 2 == 0 else x, range(1, 10))))
 
 # ex 6
-[print(x) if x%7!=0 else print("boom") for x in range(1,99)]
+[print(x) if x % 7 != 0 else print("boom") for x in range(1, 99)]
 print("-")
-print(list(map(lambda x: x if x%7==0 print("boom") else x , range(1,9))))
+print(list(map(lambda x: "boom" if x % 7 == 0 else x, range(1, 15))))
 
 # ex 7
+sum = lambda x, y: x + y
+print(sum(10, 1))
+# she codes
+
+# ex8
+print([(x,y) for x in range(1,7) for y in range(1,9)])
+# she-codes
+
