540ae70105b22a3f7e6d7bab9967b9542631da5d
shay rozner <shayrozner@gmail.com>
2022-06-26 15:41:54 +0300

    3rd

--- class10_1.py
+++ class10_1.py
@@ -8,24 +8,18 @@ def is_anagram(s,t):
 is_anagram("listen", "silent")
 is_anagram("car", "cat")
 
-class Solution(object):
-   def romanToInt(self, s):
-      """
-      :type s: str
-      :rtype: int
-      """
-      roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-      i = 0
-      num = 0
-      while i < len(s):
-         if i+1<len(s) and s[i:i+2] in roman:
-            num+=roman[s[i:i+2]]
-            i+=2
-         else:
-            #print(i)
-            num+=roman[s[i]]
-            i+=1
-      return num
-ob1 = Solution()
-print(ob1.romanToInt("III"))
-print(ob1.romanToInt("CDXLIII"))
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+def num2roman(num):
+
+    roman = ''
+
+    while num > 0:
+        for i, r in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+
+    print(roman)
+num2roman(1184)
+# google way- want to do it on my own
